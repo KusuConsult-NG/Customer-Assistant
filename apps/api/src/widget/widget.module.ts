3eece1597c4ecc45f917f6ebb3ada42d4147f9f7
KusuConsult-NG <steviekusu@gmail.com>
2026-08-03 16:52:02 +0100

    fix(api): Import WebhooksModule into WidgetModule to resolve NestJS dependency injection error for WebhookDispatcherService on Render deploy

--- apps/api/src/widget/widget.module.ts
+++ apps/api/src/widget/widget.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WidgetController } from './widget.controller';
 import { WidgetService } from './widget.service';
+import { WebhooksModule } from '../webhooks/webhooks.module';
 
 @Module({
+  imports: [WebhooksModule],
   controllers: [WidgetController],
   providers: [WidgetService],
-  exports: [WidgetService]
+  exports: [WidgetService],
 })
 export class WidgetModule {}
